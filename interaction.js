4bf033f63a5af9a0d3361432a8986672e4509320
saelyne <saelyne@kaist.ac.kr>
2022-05-30 17:14:15 +0900

    first version

--- interaction.js
+++ interaction.js
@@ -8,3 +8,21 @@ let onClickNavButton = () => {
         navmenus.style.display = "flex";
     }
 };
+
+var coll = document.getElementsByClassName("notice-title");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    console.log(content.style.display);
+    if (content.style.display === "" || content.style.display === "none"){
+        content.style.display = "block";
+    //   content.style.maxHeight = null;
+    } else {
+    //   content.style.maxHeight = content.scrollHeight + "px";
+        content.style.display = "none";
+    } 
+  });
+}
