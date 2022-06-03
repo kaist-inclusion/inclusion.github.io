1148649969a3b35fb61a4cb7f59c39793dcaf6ca
saelyne <saelyne@kaist.ac.kr>
2022-06-04 00:32:15 +0900

    minor

--- interaction.js
+++ interaction.js
@@ -15,14 +15,17 @@ var i;
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
+    var icon = this.children[0];
     var content = this.nextElementSibling;
     // console.log(content.style.display);
     if (content.style.display === "" || content.style.display === "block"){
         content.style.display = "none";
+        icon.classList.replace('fa-caret-up', 'fa-caret-down');
     //   content.style.maxHeight = null;
     } else {
     //   content.style.maxHeight = content.scrollHeight + "px";
         content.style.display = "block";
+        icon.classList.replace('fa-caret-down', 'fa-caret-up');
     } 
   });
 }
