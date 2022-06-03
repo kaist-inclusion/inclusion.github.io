3e4af1527550194ea098736caa5efd7862c57f33
saelyne <saelyne@kaist.ac.kr>
2022-06-03 22:31:18 +0900

    created sub-menu for news

--- interaction.js
+++ interaction.js
@@ -16,13 +16,13 @@ for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
-    console.log(content.style.display);
-    if (content.style.display === "" || content.style.display === "none"){
-        content.style.display = "block";
+    // console.log(content.style.display);
+    if (content.style.display === "" || content.style.display === "block"){
+        content.style.display = "none";
     //   content.style.maxHeight = null;
     } else {
     //   content.style.maxHeight = content.scrollHeight + "px";
-        content.style.display = "none";
+        content.style.display = "block";
     } 
   });
 }
